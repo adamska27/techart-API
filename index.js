2a77301cae2be0ddac40402ef5b784a968e474e5
adamska27 <adams.roy27@gmail.com>
2018-04-19 13:04:41 +0200

    (index): update the file: add middlewares, controller auth and a basic 404 page

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3005;
+const morgan = require('morgan');
+const port = process.env.PORT || 3005;
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/auth', require('./controllers/auth'));
+
+app.all('*', (req, res) => res.status(404).send('Je suis la 404'));
 
 app.listen(port, () => {
 	console.log(`the app is listening on the port ${port}`);
