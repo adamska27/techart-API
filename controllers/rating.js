47f55e62e4080a369bb73150e733ef6211793cf4
adamska27 <adams.roy27@gmail.com>
2018-07-15 12:12:25 +0200

    (rating): use the checkToken middleware + add route to get the ratings of a user + add route to get the ratings average for a product

--- controllers/rating.js
+++ controllers/rating.js
@@ -1,10 +1,14 @@
 const express = require('express'),
 	Rating = require('../models/rating');
+const sequelize = require('../config/db');
+
+const { checkToken } = require('../auth/jwt');
 
 module.exports = express
 	.Router()
-	.post('/:userId/:productId', async (req, res) => {
-		const { userId, productId } = req.params;
+	.post('/:productId', checkToken, async (req, res) => {
+		console.log('req: ', req.user);
+		const { productId } = req.params;
 		const {
 			story,
 			feeling,
@@ -20,7 +24,7 @@ module.exports = express
 
 		try {
 			await Rating.create({
-				user_id: userId,
+				user_id: req.user.id,
 				product_id: productId,
 				story,
 				feeling,
@@ -36,5 +40,70 @@ module.exports = express
 			res.status(200).send('rating created');
 		} catch (err) {
 			res.status(500).send(err);
+		}
+	})
+	.get('/:productId', checkToken, async (req, res) => {
+		const { productId } = req.params;
+		try {
+			const response = await Rating.findAll({
+				raw: true,
+				where: {
+					user_id: req.user.id,
+					product_id: productId
+				}
+			});
+			const noResponse = [];
+			if (response.length) {
+				const values = Object.values(response[0]);
+				values.length = 10;
+				res.status(200).send(values);
+			} else {
+				res.status(200).send(noResponse);
+			}
+		} catch (err) {
+			res.status(500).send(err);
+		}
+	})
+	.get('/average/:productId', async (req, res) => {
+		const { productId } = req.params;
+		try {
+			const response = await Rating.findAll({
+				raw: true,
+				attributes: [
+					[sequelize.fn('AVG', sequelize.col('story')), 'total_story'],
+					[sequelize.fn('AVG', sequelize.col('feeling')), 'total_feeling'],
+					[
+						sequelize.fn('AVG', sequelize.col('levelDesign')),
+						'total_levelDesign'
+					],
+					[sequelize.fn('AVG', sequelize.col('artDesign')), 'total_artDesign'],
+					[
+						sequelize.fn('AVG', sequelize.col('originality')),
+						'total_originality'
+					],
+					[
+						sequelize.fn('AVG', sequelize.col('soundDesign')),
+						'total_soundDesign'
+					],
+					[sequelize.fn('AVG', sequelize.col('textures')), 'total_textures'],
+					[sequelize.fn('AVG', sequelize.col('framerate')), 'total_framerate'],
+					[sequelize.fn('AVG', sequelize.col('physics')), 'total_physics'],
+					[sequelize.fn('AVG', sequelize.col('lighting')), 'total_lighting']
+				],
+				where: {
+					product_id: productId
+				}
+			});
+			const noResponse = [];
+			if (response.length) {
+				const values = Object.values(response[0]);
+				const formatedValues = values.map(x => Number(x).toFixed(1));
+				console.log({ formatedValues });
+				res.status(200).send(formatedValues);
+			} else {
+				res.status(200).send(noResponse);
+			}
+		} catch (err) {
+			res.status('500').send(err);
 		}
 	});
