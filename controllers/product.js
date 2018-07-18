91c102978ac24613802520315c0f11d07b857233
adamska27 <adams.roy27@gmail.com>
2018-07-18 16:34:01 +0200

    feat(product): add fields corresponding to data (game fetch from igdb api) in model + update get controller + add get request to get data game

--- controllers/product.js
+++ controllers/product.js
@@ -1,14 +1,68 @@
 const express = require('express'),
 	Product = require('../models/product');
 
-module.exports = express.Router().post('/:id', async (req, res) => {
-	const { id } = req.params;
-	try {
-		await Product.create({
-			id
-		});
-		res.status(200).send('product created');
-	} catch (err) {
-		res.status(500).send(err);
-	}
-});
+module.exports = express
+	.Router()
+	.post('/:id', async (req, res) => {
+		const { id } = req.params;
+		const {
+			artworks,
+			developers,
+			expansions,
+			franchises,
+			game_modes,
+			genres,
+			hypes,
+			keywords,
+			name,
+			platforms,
+			popularity,
+			publishers,
+			release_dates,
+			screenshots,
+			storyline,
+			summary,
+			themes,
+			videos
+		} = req.body;
+		try {
+			const result = await Product.create({
+				artworks,
+				developers,
+				expansions,
+				franchises,
+				game_modes,
+				genres,
+				hypes,
+				id,
+				keywords,
+				name,
+				platforms,
+				popularity,
+				publishers,
+				release_dates,
+				screenshots,
+				storyline,
+				summary,
+				themes,
+				videos
+			});
+			res.status(200).send(result);
+		} catch (err) {
+			res.status(500).send(err);
+		}
+	})
+	.get('/:id', async (req, res) => {
+		const { id } = req.params;
+
+		try {
+			const product = await Product.findOne({
+				raw: true,
+				where: { id }
+			});
+			const result = [product];
+			res.status('200').send(result);
+		} catch (err) {
+			console.log(err);
+		}
+	});
--- models/product.js
+++ models/product.js
@@ -8,6 +8,61 @@ const Product = sequelize.define(
 			type: Sequelize.INTEGER,
 			allowNull: false,
 			primaryKey: true
+		},
+		name: {
+			type: Sequelize.STRING,
+			allowNull: true
+		},
+		summary: {
+			type: Sequelize.TEXT
+		},
+		screenshots: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		storyline: {
+			type: Sequelize.TEXT
+		},
+		release_dates: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		hypes: {
+			type: Sequelize.INTEGER
+		},
+		popularity: {
+			type: Sequelize.DOUBLE
+		},
+		genres: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		franchises: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		game_modes: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		developers: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		keywords: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		themes: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		platforms: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		expansions: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		artworks: {
+			type: Sequelize.JSON
+		},
+		videos: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
+		},
+		publishers: {
+			type: Sequelize.ARRAY(Sequelize.JSON)
 		}
 	},
 	{
