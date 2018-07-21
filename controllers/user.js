e803ebf6dd5acfe4b556d2036506a6df5cd884c7
adamska27 <adams.roy27@gmail.com>
2018-07-21 15:32:01 +0200

    feat(user): create controller to get infos profile of a user + create controller to get the collection of a user

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,29 @@
 const express = require('express'),
-	User = require('../models/user');
+	User = require('../models/user'),
+	sequelize = require('../config/db');
+
+const { checkToken } = require('../auth/jwt');
 
 module.exports = express
 	.Router()
+	.get('/profile', checkToken, async (req, res) => {
+		const { id } = req.user;
+		try {
+			const result = await User.findById(id, {
+				attributes: [
+					'firstName',
+					'lastName',
+					'userName',
+					'profilePicture',
+					'email'
+				],
+				raw: true
+			});
+			res.status(200).send(result);
+		} catch (error) {
+			res.status(500).json({ error });
+		}
+	})
 	.get('/all', async (req, res) => {
 		try {
 			const users = await User.findAll({
@@ -25,5 +46,23 @@ module.exports = express
 			res.status(200).send(latestUsers);
 		} catch (err) {
 			res.status(500).send(err);
+		}
+	})
+	.get('/collection', checkToken, async (req, res) => {
+		try {
+			const { id } = req.user;
+			const collection = await sequelize
+				.query(
+					`SELECT products.name, products.artworks
+					FROM ratings
+					INNER JOIN products 
+					ON product_id = products.id
+					WHERE ratings.user_id = ${id};`,
+					{ type: sequelize.QueryTypes.SELECT }
+				)
+				.then(collection => collection);
+			res.status(200).send(collection);
+		} catch (error) {
+			res.status(500).json({ error: error });
 		}
 	});
