dfc67802be06ef94a1f5373cb5de18c87d5bc280
adamska27 <adams.roy27@gmail.com>
2018-08-13 10:10:36 +0200

    feat(index): add /review route

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const port = process.env.PORT || 3005;
 
 if (!process.env.production) require('dotenv').config();
 
+// app.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header(
+// 		'Access-Control-Allow-Headers',
+// 		'Origin, X-Requested-With, Content-Type, Accept'
+// 	);
+// 	next();
+// });
+
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
@@ -14,6 +23,7 @@ app.use('/auth', require('./controllers/auth'));
 app.use('/product', require('./controllers/product'));
 app.use('/user', require('./controllers/user'));
 app.use('/rating', require('./controllers/rating'));
+app.use('/review', require('./controllers/review'));
 
 app.all('*', (req, res) => res.status(404).send('Je suis la 404'));
 
